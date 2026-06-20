84b31e34ffd6a3a19b138b2e649d49f4adbbae3d
Nelson Cruz Mora <nelsoncruzmora@gmail.com>
2026-06-20 13:40:47 -0500

    First Commit
    Xcode Project

---
+++ PayPhoneTestApp/PayPhoneTest/PayPhoneTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayPhoneTestApp.swift
+//  PayPhoneTest
+//
+//  Created by Nelson Cruz Mora on 20/6/26.
+//
+
+import SwiftUI
+
+@main
+struct PayPhoneTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
