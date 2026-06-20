4dfe49605c296b6c0ca6de0970c970eff4b3a734
Nelson Cruz Mora <nelsoncruzmora@gmail.com>
2026-06-20 14:30:28 -0500

    Se actualiza modelo en base a la respuesta JSON del servicio arios desde: https://jsonplaceholder.typicode.com/users

--- PayPhoneTestApp/PayPhoneTest/Models/User.swift
+++ PayPhoneTestApp/PayPhoneTest/Models/User.swift
@@ -8,6 +8,32 @@
 
 import Foundation
 
-struct User: Identifiable, Codable {
-    let id: String
+struct UserDTO: Codable, Identifiable, Hashable {
+    let id: Int
+    let name: String
+    let username: String
+    let email: String
+    let address: AddressDTO
+    let phone: String
+    let website: String
+    let company: CompanyDTO
+}
+
+struct AddressDTO: Codable, Hashable {
+    let street: String
+    let suite: String
+    let city: String
+    let zipcode: String
+    let geo: GeoDTO
+}
+
+struct GeoDTO: Codable, Hashable {
+    let lat: String
+    let lng: String
+}
+
+struct CompanyDTO: Codable, Hashable {
+    let name: String
+    let catchPhrase: String
+    let bs: String
 }
